6317c52246e30894aabd7015f36e93ab3a318d28
Alex White <alexlloydwhite@gmail.com>
2021-05-24 17:33:50 -0500

    appbar init create

---
+++ src/components/NavBar/Drawer.jsx
@@ -0,0 +1,9 @@
+import MenuIcon from '@material-ui/icons/Menu';
+
+const Drawer = () => {
+    return (  
+        <MenuIcon />
+    );
+}
+ 
+export default Drawer;
--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,47 +1,42 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import {useSelector} from 'react-redux';
+import Drawer from './Drawer';
 
-function Nav() {
-  const user = useSelector((store) => store.user);
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
-  let loginLinkData = {
-    path: '/login',
-    text: 'Login / Register',
-  };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-  if (user.id != null) {
-    loginLinkData.path = '/user';
-    loginLinkData.text = 'Home';
-  }
+const Nav = () => {
+  const classes = useStyles();
 
-  return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
-
-        {user.id && (
-          <>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
-  );
+  return (  
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <Drawer />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            CellarDex
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>  );
 }
-
+ 
 export default Nav;
