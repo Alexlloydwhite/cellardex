0dd86f6ff792f37882bc271a9c5bb6fc97c9fb24
Alex White <alexlloydwhite@gmail.com>
2021-05-24 18:52:02 -0500

    NavBar displays options, logo, directs to correct page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -31,68 +31,79 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
         <Switch>
-          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-          <Redirect exact from="/" to="/home" />
+          <Nav>
+            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+            <Redirect exact from="/" to="/home" />
 
-          {/* Visiting localhost:3000/about will show the about page. */}
-          <Route
-            // shows AboutPage at all times (logged in or not)
-            exact
-            path="/about"
-          >
-            <AboutPage />
-          </Route>
-
-          {/* For protected routes, the view could show one of several things on the same route.
+            {/* Visiting localhost:3000/about will show the about page. */}
+            <Route
+              // shows AboutPage at all times (logged in or not)
+              exact
+              path="/about"
+            >
+              <AboutPage />
+            </Route>
+            {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/user will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the LoginPage (component).
             Even though it seems like they are different pages, the user is always on localhost:3000/user */}
-          <ProtectedRoute
-            // logged in shows UserPage else shows LoginPage
-            exact
-            path="/search"
-          >
-            <SearchView />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // logged in shows UserPage else shows LoginPage
+              exact
+              path="/search"
+            >
+              <SearchView />
+            </ProtectedRoute>
 
-          {/* When a value is supplied for the authRedirect prop the user will
+            <ProtectedRoute
+              // with authRedirect:
+              // - if logged in, redirects to "/search"
+              // - else shows LandingPage at "/home"
+              exact
+              path="/profile"
+            >
+              <UserPage />
+            </ProtectedRoute>
+
+            {/* When a value is supplied for the authRedirect prop the user will
             be redirected to the path supplied when logged in, otherwise they will
             be taken to the component and path supplied. */}
-          <ProtectedRoute
-            // with authRedirect:
-            // - if logged in, redirects to "/user"
-            // - else shows LoginPage at /login
-            exact
-            path="/login"
-            authRedirect="/search"
-          >
-            <LoginPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // with authRedirect:
+              // - if logged in, redirects to "/user"
+              // - else shows LoginPage at /login
+              exact
+              path="/login"
+              authRedirect="/search"
+            >
+              <LoginPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // with authRedirect:
-            // - if logged in, redirects to "/user"
-            // - else shows RegisterPage at "/registration"
-            exact
-            path="/registration"
-            authRedirect="/search"
-          >
-            <RegisterPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // with authRedirect:
+              // - if logged in, redirects to "/user"
+              // - else shows RegisterPage at "/registration"
+              exact
+              path="/registration"
+              authRedirect="/search"
+            >
+              <RegisterPage />
+            </ProtectedRoute>
 
-          <ProtectedRoute
-            // with authRedirect:
-            // - if logged in, redirects to "/search"
-            // - else shows LandingPage at "/home"
-            exact
-            path="/home"
-            authRedirect="/search"
-          >
-            <LandingPage />
-          </ProtectedRoute>
+            <ProtectedRoute
+              // with authRedirect:
+              // - if logged in, redirects to "/search"
+              // - else shows LandingPage at "/home"
+              exact
+              path="/home"
+              authRedirect="/search"
+            >
+              <LandingPage />
+            </ProtectedRoute>
 
+
+          </Nav>
           {/* If none of the other routes matched, we will show a 404. */}
           <Route>
             <h1>404</h1>
--- src/components/NavBar/Drawer.jsx
+++
@@ -1,9 +0,0 @@
-import MenuIcon from '@material-ui/icons/Menu';
-
-const Drawer = () => {
-    return (  
-        <MenuIcon />
-    );
-}
- 
-export default Drawer;
