959b96e1161e2d673f4657ea03848244ac0bbfd9
Alex White <alexlloydwhite@gmail.com>
2021-05-26 14:35:37 -0500

    starting route to add save apairing to DB

--- src/components/PairingDescription/PairingBody.jsx
+++ src/components/PairingDescription/PairingBody.jsx
@@ -2,24 +2,32 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 
 const PairingBody = ({ item }) => {
 
     const history = useHistory();
+    const dispatch = useDispatch();
+    const user = useSelector(store => store.user)
 
-    const handleAddClick = () => {
+    const handleAddClick = (id) => {
         console.log('Clicked! Add to my pairings btn');
+        // send user to profile view
         history.push('/profile');
+        // Save pairing to user
+        dispatch({ type: 'SET_SAVED_PAIRING', userId: user.id, pairingId: id });
     }
 
     return (
         <Container style={{ marginTop: 50, marginBottom: 25 }}>
+            {/* Pairing Description */}
             <Typography 
                 style={{ marginBottom: 15 }}
                 variant="h5"
             >
                 {item.description}
             </Typography>
+            {/* Back button takes user back home*/}
             <Button
                 onClick={() => history.push('/')}
                 color="Secondary"
@@ -28,8 +36,9 @@ const PairingBody = ({ item }) => {
             >
                 Back
             </Button>
+            {/* Btn adds pairing to user's saved pairings, sends user to profile view */}
             <Button
-                onClick={handleAddClick}
+                onClick={() => handleAddClick(item.id)}
                 color="primary"
                 variant="contained"
             >
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -2,8 +2,9 @@ import { all, takeEvery } from 'redux-saga/effects';
 import loginSaga from './login.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
-import pairingSaga from './fetchpairing.saga';
+import pairingSaga from './fetchPairing.saga';
 import getPairingById from './fetchPairingById.saga';
+import setSavedPairing from './setSavedPairing.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -15,6 +16,7 @@ import getPairingById from './fetchPairingById.saga';
 export default function* rootSaga() {
   yield takeEvery('FETCH_PAIRING', pairingSaga);
   yield takeEvery('SET_PAIRING_CLICK', getPairingById);
+  yield takeEvery('SET_SAVED_PAIRING', setSavedPairing);
   yield all([
     loginSaga(), // login saga is now registered
     registrationSaga(),
